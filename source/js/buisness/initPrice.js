0e59afca412636107d69336d2246ddc4ca732680
restby <restby@gmail.com>
2023-10-18 14:45:07 +0300

    js правки цены

--- source/js/buisness/initPrice.js
+++ source/js/buisness/initPrice.js
@@ -5,19 +5,36 @@ const getPriceData = () => {
   console.log(itemsData);
 
   if (document.querySelector('[data-catalog__element]')) {
-    const catalogBoats = document.querySelectorAll('[data-catalog__element]');
-    catalogBoats.forEach((el) => {
+    const catalogElements = document.querySelectorAll('[data-catalog__element]');
+    catalogElements.forEach((el) => {
       el.addEventListener('click', (evt) => {
         let target = evt.target;
         let itemDataValue = null;
-        while (target && target !== catalogBoats) {
+        while (target && target !== catalogElements) {
           if (target.getAttribute('data-open-modal')) {
             itemDataValue = target.getAttribute('data-open-modal');
             break;
           }
           target = target.parentNode;
         }
-        console.log(`Значение itemDataValue за пределами цикла: ${itemDataValue}`);
+
+        const myObject = {
+          key1: 'value1',
+          key2: 'value2',
+          key3: 'value3'
+        };
+
+        const targetValue = 'value2'; // Значение, с которым сравниваем ключи
+
+        for (const key in myObject) {
+          if (myObject.hasOwnProperty(key)) { // Убедимся, что ключ принадлежит самому объекту, а не его прототипу
+            if (myObject[key] === targetValue) {
+              console.log(`Найден ключ с нужным значением: ${key}`);
+            }
+          }
+        }
+
+
       });
     });
   }
