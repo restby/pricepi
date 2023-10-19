56fe3f685f0195bf437779737c91f649a4bc4596
restby <restby@gmail.com>
2023-10-19 09:08:38 +0300

    правки по js

--- source/js/buisness/initPrice.js
+++ source/js/buisness/initPrice.js
@@ -1,11 +1,13 @@
 import {items} from './priceData';
 
 const getPriceData = () => {
+
   const itemsData = Object.keys(items);
   console.log(itemsData);
 
   if (document.querySelector('[data-catalog__element]')) {
     const catalogElements = document.querySelectorAll('[data-catalog__element]');
+
     catalogElements.forEach((el) => {
       el.addEventListener('click', (evt) => {
         let target = evt.target;
@@ -18,22 +20,20 @@ const getPriceData = () => {
           target = target.parentNode;
         }
 
-        // console.log(itemDataValue);
         if (items.hasOwnProperty(itemDataValue)) {
-          // Если ключ существует, получите вложенный объект
-          const innerObject = items[itemDataValue];
-
-          // Теперь вы можете получить ключи и значения вложенного объекта и сохранить их в переменные
-          const keys = Object.keys(innerObject);
-          const values = Object.values(innerObject);
-
-          // Далее вы можете использовать keys и values по вашему усмотрению
+          const itemDataObject = items[itemDataValue];
+          const keys = Object.keys(itemDataObject);
+          const values = Object.values(itemDataObject);
           console.log('Keys:', keys);
           console.log('Values:', values);
         } else {
-          // Если ключ не найден, обработайте это по вашему усмотрению
           console.log('Ключ не найден');
         }
+
+        const modalItem = document.querySelector(`[data-modal=${itemDataValue}]`);
+        console.log(modalItem);
+
+
       });
     });
   }
