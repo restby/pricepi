ac936562dd3e866c450b14fa8d09bdc2ae973af8
restby <restby@gmail.com>
2023-10-20 09:20:25 +0300

    js test

--- source/js/buisness/initCardItem.js
+++ source/js/buisness/initCardItem.js
@@ -9,33 +9,195 @@
 
 // body.appendChild(fragment);
 
+// const initCardItem = () => {
+
+//   const body = document.querySelector('body');
+//   const templateFragment = document.querySelector('[data-modal-template]').content;
+//   // Находим фрагмент с содержимым темплейта
+//   const template = templateFragment.querySelector('[data-modal--preload]'); // В фрагменте находим нужный элемент
+
+//   const fragment = document.createDocumentFragment();
+//   const element = template.cloneNode(true);
+//   fragment.appendChild(element);
+
+//   if (document.querySelector('[data-catalog__element]')) {
+//     const catalogElements = document.querySelectorAll('[data-catalog__element]');
+
+//     catalogElements.forEach((el) => {
+//       el.addEventListener('click', (evt) => {
+//         let target = evt.target;
+//         let itemDataValue = null;
+//         while (target && target !== catalogElements) {
+//           if (target.getAttribute('data-open-modal')) {
+//             body.appendChild(fragment);
+//             break;
+//           }
+//           target = target.parentNode;
+//         }
+//       });
+//     });
+//   }
+// };
+// export {initCardItem};
+
+
 const initCardItem = () => {
+  if (document.querySelector('[data-dimensions]')) {
+console.log('11111');
 
-  const body = document.querySelector('body');
-  const templateFragment = document.querySelector('[data-modal-template]').content;
-  // Находим фрагмент с содержимым темплейта
-  const template = templateFragment.querySelector('[data-modal--preload]'); // В фрагменте находим нужный элемент
+  };
 
-  const fragment = document.createDocumentFragment();
-  const element = template.cloneNode(true);
-  fragment.appendChild(element);
+  if (document.querySelector('[data-full-load-capacity]')) {
+console.log('11111');
 
-  if (document.querySelector('[data-catalog__element]')) {
-    const catalogElements = document.querySelectorAll('[data-catalog__element]');
+  };
 
-    catalogElements.forEach((el) => {
-      el.addEventListener('click', (evt) => {
-        let target = evt.target;
-        let itemDataValue = null;
-        while (target && target !== catalogElements) {
-          if (target.getAttribute('data-open-modal')) {
-            body.appendChild(fragment);
-            break;
-          }
-          target = target.parentNode;
-        }
-      });
-    });
-  }
+  if (document.querySelector('[data-curb-weight]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-count-spring-leaves]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-clearance]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-boat-dimensions]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-dump-truck-function]')) {
+console.log('11111');
+
+  };
+
+
+  if (document.querySelector('[data-item-option="noWheel"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-option="noWheelH300"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-option="noWheelH600"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-option="noWheelH1220"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-option="noWheelH1220C"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-option="wheel2R13"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-option="wheel2R13H300"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-option="wheel2R13H600"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-option="wheel2R13H1220"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-option="wheel2R13H1220C"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-option="wheel4R13H1220"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-option="wheel4R13H1220C"]')) {
+console.log('11111');
+
+  };
+
+
+  if (document.querySelector('[data-item-price="noWheel"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-price="noWheelH300"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-price="noWheelH600"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-price="noWheelH1220"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-price="noWheelH1220C"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-price="wheel2R13"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-price="wheel2R13H300"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-price="wheel2R13H600"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-price="wheel2R13H1220"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-price="wheel2R13H1220C"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-price="wheel4R13H1220"]')) {
+console.log('11111');
+
+  };
+
+  if (document.querySelector('[data-item-price="wheel4R13H1220C"]')) {
+console.log('11111');
+
+  };
 };
-export {initCardItem};
+
+// export {initCardItem};
